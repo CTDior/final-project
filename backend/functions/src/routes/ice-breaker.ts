17144fe9358acc9035704d4b5a6a92ab7de7b28a
CTDior <83152946+CTDior@users.noreply.github.com>
2021-10-08 09:06:11 -0400

    get route

--- backend/functions/src/routes/ice-breaker.ts
+++ backend/functions/src/routes/ice-breaker.ts
@@ -1,15 +1,18 @@
-// import express from "express";
-// import { Group, GroupMember } from "../models/IceBreaker";
-// import  { getClient } from "../db"
+import express from "express";
+import { Group } from "../models/IceBreaker";
+import  { getClient } from "../db"
 
 
-// const routes = express.Router();
+const routes = express.Router();
 
-// routes.get("/groups", async (req, res) => {
-//     try {
-//         const client = await getClinet();
-//         console.log(client.db().databaseName)
-//         const results = await client.db().collection<Group>("icebreaker").find().toArray();
-//     }
-
-// })
+routes.get("/groups", async (req, res) => {
+    try {
+        const client = await getClient();
+        console.log(client.db().databaseName)
+        const results = await client.db().collection<Group>("groups").find().toArray();
+        res.json(results);
+    } catch (err) {
+        console.error("ERROR", err);
+        res.status(500).json( { message: "Internal Server Error"} )
+    }
+});
