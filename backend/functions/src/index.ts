2c7cd25d94e28672e1db5889e37d8b1a0c56e15a
Emilie Tole <eetole@gmail.com>
2021-10-08 11:07:14 -0400

    added and tested routes

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,12 +1,12 @@
-import * as functions from 'firebase-functions';
-import express from 'express';
-import cors from 'cors';
-// import exampleRoutes from './routes/example';
+import * as functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import { routes } from "./routes/ice-breaker";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.use("/", exampleRoutes);
+app.use("/", routes);
 
 export const api = functions.https.onRequest(app);
